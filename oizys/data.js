33c5b7ba3dca01b5415a3774991a0d7cfc12892b
Illedy <almacen.illedy@gmail.com>
2026-01-30 00:11:44 +0100

    finished reestructuring, prepared for mission added later

---
+++ oizys/data.js
@@ -0,0 +1,57 @@
+const data={
+    "a":[
+        {
+            "n":"Placeholder A Mission Name",
+            "b":"Placeholder Big Fish Name",
+            "g":[
+                "Placeholder instruction 1.",
+                "Placeholder instruction 2.",
+                "Placeholder instruction 3.",
+            ],
+        }
+    ],
+    "w":[
+        {
+            "n":"Placeholder Weather Mission Name",
+            "b":"Placeholder Big Fish Name",
+            "g":[
+                "Placeholder instruction 1.",
+                "Placeholder instruction 2.",
+                "Placeholder instruction 3.",
+            ],
+        }
+    ],
+    "t":[
+        {
+            "n":"Placeholder Time Restricted Mission Name",
+            "b":"Placeholder Big Fish Name",
+            "g":[
+                "Placeholder instruction 1.",
+                "Placeholder instruction 2.",
+                "Placeholder instruction 3.",
+            ],
+        }
+    ],
+    "s":[
+        {
+            "n":"Placeholder Sequential Mission Name",
+            "b":"Placeholder Big Fish Name",
+            "g":[
+                "Placeholder instruction 1.",
+                "Placeholder instruction 2.",
+                "Placeholder instruction 3.",
+            ],
+        }
+    ],
+        "c":[
+        {
+            "n":"Placeholder Critical Mission Name",
+            "b":"Placeholder Big Fish Name",
+            "g":[
+                "Placeholder instruction 1.",
+                "Placeholder instruction 2.",
+                "Placeholder instruction 3.",
+            ],
+        }
+    ]
+};
