14c54f0f022e35c38d012a434f10a2858a493892
Illedy <almacen.illedy@gmail.com>
2026-02-01 05:24:14 +0100

    preliminary notes for oizys

--- oizys/data.js
+++ oizys/data.js
@@ -1,24 +1,85 @@
 const data={
     "a":[
         {
+            "n":"Collapsed Tunnel Specimen Survey",
+            "b":"Collapsed sucker",
+            "g":[
+                "bait big, Slap !!/identicast, multihook, triple hook !!!" 
+            ]
+        },
+        {
+            "n":"Red Cosmomaggot Testing",
+            "b":"Magnetite Skink",
+            "g":[
+                "patience, mooch !, x2 if !!, !!! is powerful" 
+            ]
+        },
+        {
+            "n":"Edible Marine Life Survey",
+            "b":"Ancient Oizys Fossil",
+            "g":[
+                "no bait, multihook 2, identicast !!! into miltihook + triple hook. Time late !!?" 
+            ]
+        },
+        {
+            "n":"Moros Well Environmental Survey",
+            "b":"Moros Horned Viper",
+            "g":[
+                "patience, bait big, hook all, stellar + identicast !!!" 
+            ]
+        },
+        {
+            "n":"Valuable Marine Specimens",
+            "b":"Steel Longnose",
+            "g":[
+                "patience, biat big, stellar !!!, normal hook the rest" 
+            ]
+        },
+        {
+            "n":"Marine Distribution Survey",
+            "b":"Oddweight Ironsand Lizard",
+            "g":[
+                "bait big, identicast Pseudolumpsucker for intuition, bait big, no size needed" 
+            ]
+        },
+        {
+            "n":"EX: Large Mutant Distribution Survey",
+            "b":"Elongated Cameroceras",
+            "g":[
+                "lure message !!!, slap the other !!!" 
+            ]
+        },
+        {
+            "n":"EX: Dune Resource Emergency I",
+            "b":"Oddweight Ironsand Lizard",
+            "g":[
+                "patience, bait small, mooch, !!! is powerful. Identicast mooch and spare?" 
+            ]
+        },
+        {
             "n":"Placeholder A Mission Name",
             "b":"Placeholder Big Fish Name",
             "g":[
                 "Placeholder instruction 1.",
                 "Placeholder instruction 2.",
                 "Placeholder instruction 3.",
-            ],
+            ]
         }
     ],
     "w":[
         {
-            "n":"Placeholder Weather Mission Name",
-            "b":"Placeholder Big Fish Name",
+            "n":"EX: Gravitational Sphere Impact (Clear Skies)",
+            "b":"Marine Magnet",
             "g":[
-                "Placeholder instruction 1.",
-                "Placeholder instruction 2.",
-                "Placeholder instruction 3.",
-            ],
+                "bait big, slap !!, prize catch message !!! High perception" 
+            ]
+        },
+        {
+            "n":"EX+: Cosmokrill Testing",
+            "b":"Pseudosaurus",
+            "g":[
+                "prize catch, bait big, slap !!, identicast !!! need more perception (food?)" 
+            ]
         }
     ],
     "t":[
@@ -29,7 +90,7 @@ const data={
                 "Placeholder instruction 1.",
                 "Placeholder instruction 2.",
                 "Placeholder instruction 3.",
-            ],
+            ]
         }
     ],
     "s":[
@@ -40,7 +101,7 @@ const data={
                 "Placeholder instruction 1.",
                 "Placeholder instruction 2.",
                 "Placeholder instruction 3.",
-            ],
+            ]
         }
     ],
         "c":[
@@ -51,7 +112,15 @@ const data={
                 "Placeholder instruction 1.",
                 "Placeholder instruction 2.",
                 "Placeholder instruction 3.",
-            ],
+            ]
         }
     ]
 };
+
+const ph={
+            "n":"EX: Gravitational Sphere Impact (Clear Skies)",
+            "b":"Marine Magnet",
+            "g":[
+                "bait big, slap !!, prize catch message !!! High perception" 
+            ]
+        }
