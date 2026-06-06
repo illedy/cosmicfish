f14e41e5a8806b5200fe9e655ebc3bdaf92424da
Illedy <almacen.illedy@gmail.com>
2026-06-07 00:02:57 +0200

    auxesia phase 1 added

--- auxesia/data.js
+++ auxesia/data.js
@@ -1,68 +1,91 @@
 const data={
     "a":[
         {
-            "n":"???",
-            "b":"???",
+            "n":"Emerald Springs Specimen Survey",
+            "b":"Topiary Megagar",
             "g":[
-                "???"
+                "Bait with Ambitious Lure, hooking only !!! bites.",
+                "Upon catching <arowana analog>, Identical Cast, Multihook and Double Hook.",
+                "Upon receiving Intuition, Surface Slap and bait with Ambitious Lure hooking only !!! bites."
             ],
         },
         {
-            "n":"???",
-            "b":"???",
+            "n":"Freshwater Arthrolure Texting",
+            "b":"Cosmoss",
             "g":[
-                "???"
+                "Keep Patience II active.",
+                "Bait with Modest Lure, hooking only ! bites.",
+                "Upon catching <cosmoss>, Identical Cast."
             ],
         },
         {
-            "n":"???",
-            "b":"???",
+            "n":"Aquatic Mechanism Operational Survey",
+            "b":"Nervure corntail",
             "g":[
-                "???"
+                "Bait with Ambitious Lure, hooking everything.",
+                "Upon catching <fishform: aquamotion> or <nervure corntail>, Identical Cast, Multihook and Triple Hook.",
+                "After triple hooking, Chum and Multihook all possible casts, and stop using lure actions."
             ],
         },
         {
-            "n":"???",
-            "b":"???",
+            "n":"Emerald Springs Environmental Survey",
+            "b":"Eupseudohedron",
             "g":[
-                "???"
+                "Bait with Ambitious Lure, hooking everything.",
+                "Upon catching <eupseudohedron>, Identical Cast, Prize Catch and Stellar Hookset."
             ],
         },
         {
-            "n":"???",
-            "b":"???",
+            "n":"Valuable Northern-central Specimens",
+            "b":"Fishform: Aquapure",
             "g":[
-                "???"
+                "Keep Patience II actie.",
+                "Bait with Modest Lure, hooking only ! bites.",
+                "Upon catching <stacks foliafin>, Mooch and Stellar Hookset !!! bites.",
+                "Repeat."
             ],
         },
         {
-            "n":"???",
-            "b":"???",
+            "n":"Aquatic Mechanism Distribution Survey",
+            "b":"Orchidelirium principal",
             "g":[
-                "???"
+                "Keep Patience II active.",
+                "Bait with Modest Lure, hooking only ! bites.",
+                "Mooch when applicable, hooking everything.",
+                "Big fish does not need to be caught as large."
             ],
         },
         {
-            "n":"???",
-            "b":"???",
+            "n":"EX: Northern-central Specimen Survey",
+            "b":"Grand green guppie",
             "g":[
-                "???"
+                "Chum all casts when GP>350",
+                "Bait with Ambitious Lure, hooking everything.",
+                "Get <swell>x3 (check exact name, 1 cricket might be necessary).",
+                "Upon receiving Intuition, start baiting with Modest Lure hooking only ! bites.",
+                "Surface slap if the catch is not a big fish."
             ],
         },
         {
-            "n":"???",
-            "b":"???",
+            "n":"EX: Large Aquatic Organism Distribution Survey",
+            "b":"Topiary carp",
             "g":[
-                "???"
+                "Prize Catch.",
+                "Bait with Ambitious Lure, hooking only one !! or !!! bite.",
+                "If it was !!, Surface Slap, Prize Catch and start hooking only !!! bites."
             ],
         }
     ],
     "w":[
         {
-            "n":"???",
-            "b":"???",
+            "n":"EX+: Western Nervure Cultivation Survey (Clear Skies)",
+            "b":"Verdant veil",
             "g":[
-                "???"
+                "First Cast: Prize Catch, Multihook, Modest Lure and Triple Hook.",
+                "All Thaliak's Favors and Cordial.",
+                "Modest Lure when at max GP.",
+                "Upon catching <verdant veil>, Identical Cast, Multihook and Triple Hook. Wait GP if necessary.",
+                "Too RNG dependent, not recommended for currency farming."
             ],
         },
         {
@@ -71,7 +94,7 @@ const data={
             "g":[
                 "???"
             ],
-        }
+        },
     ],
     "t":[
         {
@@ -82,10 +105,12 @@ const data={
             ],
         },
         {
-            "n":"???",
-            "b":"???",
+            "n":"EX+: Full Bloom Fountain Specimen Survey (16-20)",
+            "b":"Topiary piracucu",
             "g":[
-                "???"
+                "Bait with Ambitious Lure, hooking only !! bites.",
+                "Surface Slap <fishform: aquaflow> every time it is caught.",
+                "Upon receiving intuition, hook only !!! bites after using Surface Slap on <fishform: aquaflow>"
             ],
         }
     ],
